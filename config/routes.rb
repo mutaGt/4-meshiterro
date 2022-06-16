319db72fc602e090dd3248d97120cef46b7cacb2
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-06-16 13:09:05 +0000

    [Finish]4-18

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+    
   resources :users, only: [:show, :edit, :update]
   
   get '/homes/about'=>'homes#about', as: 'about'
